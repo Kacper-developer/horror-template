5e79208f619fa622957527b358757cacce0783c9
HunterTheFrontEnd <hunter.the.frontend.developer@gmail.com>
2021-06-29 20:46:05 +0200

    ten update

--- assets/scripts/javascript/slide.js
+++ assets/scripts/javascript/slide.js
@@ -1,11 +1,17 @@
 const swiper = new Swiper('.swiper-container', {
-  // Optional parameters
+
   direction: 'horizontal',
   loop: true,
+
+  // pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
 
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+
 });

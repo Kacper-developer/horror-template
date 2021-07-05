2ebfe825c04688c9450db9308f81fb5482729180
HunterTheFrontEnd <hunter.the.frontend.developer@gmail.com>
2021-07-05 21:00:43 +0200

    Yay. Animations :D

---
+++ assets/scripts/javascript/scroll-reveal.js
@@ -0,0 +1,25 @@
+var slideUp = {
+    distance: '1500%',
+    origin: 'bottom',
+    opacity: null
+};
+
+
+ScrollReveal().reveal('.blog_slider-section', {
+    delay:  500,
+    duration: 1000,
+    distance: '50%'
+}, slideUp);
+
+ScrollReveal().reveal('.blog_proceed-section', {
+    delay:  750,
+    duration: 1000,
+    distance: '75%'
+}, slideUp);
+
+ScrollReveal().reveal('#about-section', {
+    delay:  1500,
+    duration: 1000,
+    distance: '25%'
+}, slideUp);
+

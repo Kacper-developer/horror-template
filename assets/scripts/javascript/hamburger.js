5200eb23d309cc3a8687f644e92ad3d8222f2027
HunterTheFrontEnd <hunter.the.frontend.developer@gmail.com>
2021-07-07 18:53:01 +0200

    new hamburger :D

--- assets/scripts/javascript/hamburger.js
+++ assets/scripts/javascript/hamburger.js
@@ -3,11 +3,24 @@ function hamburger(){
 
     var hamburger = document.getElementById('burger')
     var hamburgerMenu = document.getElementById('hamburgermenu')
+    var navigation = document.querySelector('.navbar')
 
 if(hamburger.checked){
-    hamburgerMenu.style.left = '0'
+    
+  hamburgerMenu.style.height = 'auto'
+  hamburgerMenu.style.padding = '20px 0'
+  hamburgerMenu.style.display = 'flex'
+
+  navigation.style.background = '#fff'
+    
 } else {
-    hamburgerMenu.style.left = '-100vw'
+    
+  hamburgerMenu.style.height = '0'
+  hamburgerMenu.style.padding = '0px 0'
+  hamburgerMenu.style.display = 'none'
+
+  navigation.style.background = 'transparent'
+    
 }
 
 

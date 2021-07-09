98d553847eecffa5f11bf3d688a0ed113d62699c
HunterTheFrontEnd <hunter.the.frontend.developer@gmail.com>
2021-07-09 20:35:10 +0200

    really last changes

---
+++ spooky_story_blog/assets/scripts/javascript/slide.js
@@ -0,0 +1,10 @@
+new Splide( '.splide', {
+	classes: {
+		arrows: 'splide__arrows arrow',
+		arrow : 'splide__arrow arrow',
+		prev  : 'splide__arrow--prev arrow',
+		next  : 'splide__arrow--next arrow',
+        pagination: 'splide__pagination',
+        page  : 'splide__pagination__page',
+	},
+} ).mount();
